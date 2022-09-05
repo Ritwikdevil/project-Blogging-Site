1c95645ec7b2de981c2cbc9f3db27c5bcdb61d50
prince9871 <soniprince142@gmail.com>
2022-09-05 16:36:37 +0530

    create done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-
-
-
+//const AuthorModel=require('../models/authorModel')
+const AuthorController=require('../controllers/AuthorController')
+router.post('/CreateAuthor',AuthorController.createAuthor)
 
 module.exports = router;

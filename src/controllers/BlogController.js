e2ca8542e77c2654a8b5896fe2540f2c1ac6fe81
ritwik <ritwik6927@gmail.com>
2022-09-06 12:08:02 +0530

    day1 task complete

--- src/controllers/BlogController.js
+++ src/controllers/BlogController.js
@@ -2,10 +2,28 @@ const mongoose = require('mongoose')
 const blogModel = require("../models/blogModel")
 const authorModel = require("../models/authorModel")
 
-//CreateData 
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
+}
+
+const isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0
+}
+
+const isValidObjectId = function (objectId) {
+    return mongoose.Types.ObjectId.isValid(objectId)
+}
+//Createblogsdata//post
 const createBlogs = async function (req, res) {
     try {
         let data = req.body
+        if (!isValidRequestBody(data)) {
+            res.status(400).send({ status: false, message: 'Invalid request parameters. please provide blog details' })
+            return
+        }
+
         //checking valid author id by collection
         let authorId = req.body.authorId
         let checkAuthor = await authorModel.findById(authorId)
@@ -23,37 +41,37 @@ const createBlogs = async function (req, res) {
 //Get Blog Data-- are not deleted and published
 const getBlogsData = async function (req, res) {
     try {
-        let authorid = req.query.authorId
-        let category = req.query.category
-        let tags = req.query.tags
-        let subcategory = req.query.subcategory
-        
-        if (authorid) {
-            if (!authorid) return res.status(404).send({ status: false, message: "No Doc Found" })
-            let data = await blogModel.find({ isDelete: false } && { isPublished: true } && { authorid: authorid })
-            if (data == []) return res.send({ status: false, msg: "data not available" })
-            return res.status(200).send({ status: true, data: data })
+        const filterQuery = { isDeleted: false, isPublished: true }
+        const queryParams = req.query
+        if (isValidRequestBody(queryParams)) {
+            const { authorId, category, tags, subcategory } = queryParams
+
+            if (isValid(authorId) && isValidObjectId(authorId)) {
+                filterQuery['authorId'] = authorId
+            }
+            if (isValid(category)) {
+                filterQuery['category'] = category
+            }
+
+            if (isValid(tags)) {
+                filterQuery['tags'] = tags
+            }
+            if (isValid(subcategory)) {
+                filterQuery['tags'] = subcategory
+            }
         }
-        if (category) {
-            let data = await blogModel.find({ isDelete: false } && { isPublished: true } && { category: category })
-            if (data == []) return res.send({ status: false, msg: "data not available" })
-            return res.status(200).send({ status: true, data: data })
+        const Blogs = await blogModel.find(filterQuery)
+        if (Array.isArray(Blogs) && Blogs.length === 0) {
+            res.status(404).send({ status: false, msg: "no blogs found" })
+            return
         }
-        if (tags) {
-            let data = await blogModel.find({ isDelete: false } && { isPublished: true } && { tags: tags })
-            if (data == []) return res.send({ status: false, msg: "data not available" })
-            return res.status(200).send({ status: true, data: data })
-        }
-        if (subcategory) {
-            let data = await blogModel.find({ isDelete: false } && { isPublished: true } && { subcategory: subcategory })
-            if (data == []) return res.send({ status: false, msg: "data not available" })
-            return res.status(200).send({ status: true, data: data })
+        res.status(200).send({ status: true, message: 'Blogs list', data: Blogs })
     }
-  }  catch (error) {
+    catch (error) {
         res.status(500).send({ status: false, message: error.message })
     }
 }
 
-    //data--publish,deleted
-    module.exports.createBlogs = createBlogs
-    module.exports.getBlogsData = getBlogsData
+
+module.exports.createBlogs = createBlogs
+module.exports.getBlogsData = getBlogsData
